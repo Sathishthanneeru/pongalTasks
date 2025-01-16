a66f0cebd24d45ac0f65e46183622823b273c3f9
Sathishthanneeru <sathishthanneeru36@gmail.com>
2025-01-16 20:53:32 +0530

    new

--- tables.js
+++ tables.js
@@ -1,13 +1,13 @@
 function multliplication(num) {
   console.log(`${num}`);
   for (i = 0; i <= 10; i++) {
-    console.log(`${num} x ${i} = ${num*i}`)
+    console.log(`${num} x ${i} = ${num * i}`);
   }
 }
 const input = parseInt(prompt("Enter a number:"), 10);
-if (!isNaN(input)) {
-  multliplication(input);
+if (!isNaN()) {
+  multliplication();
 } else {
-  console.log("enter valid number");
+    console.log("enter valid number");
 }
-
+console.log(multliplication(9));
